b507121e47637e0688665ab8b8f519f0364bc44b
fahrisyany <fahrisyany@gmail.com>
2020-11-04 00:02:07 +0700

    initial setup

---
+++ src/pages/app/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.scss';
+import Data from '../../components/data-comp/data.comp'
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <Data />
+    </div>
+  );
+}
+
+export default App;
